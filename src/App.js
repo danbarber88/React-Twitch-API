2e3b06dccc53ba8b2dfd2e3c16563a5c7ed7e001
Dan Barber <d.j.barber88@gmail.com>
2018-03-12 18:02:10 +0000

    messing with basics of react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Box from './Box'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      boxes: [
+        {
+          text: "frogs",
+          color: "green",
+        },
+        {
+          text: "turkey",
+          color: "pink",
+        },
+        {
+          text: "donkey",
+          color: "brown",
+        },
+      ]
+    };
+  }
+
   render() {
+    const boxes = this.state.boxes.map((box, i) => (
+      <Box 
+        key={i}
+        text={box.text}
+        color={box.color}
+      />
+    ))
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {boxes}
       </div>
     );
   }
